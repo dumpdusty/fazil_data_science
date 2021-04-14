1b503f790a8c2454adda114490231a0a9a9e099e
dumpdusty <dumpdusty@gmail.com>
2021-04-14 08:53:40 -0700

    Update Unit_2_Week_2_Project.sql
    Header added

--- Unit_2_Week_2_Project.sql
+++ Unit_2_Week_2_Project.sql
@@ -1,6 +1,12 @@
+--Unit_2_Week_2_SQL_Project
+
+--1. Which campaign tipycally has the highest cost each year?
+
+
 select  sac.campaign , sum(sad."cost" )
 from public.search_ad_campaigns sac 
 inner join public.search_ad_data sad 
 on sac.campaign_id = sad.campaign_id 
 group by sac.campaign
 order by sum(sad."cost") desc 
+
