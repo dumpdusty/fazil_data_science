742c99098c62481b4d0a7483cee1c2decb127858
dumpdusty <dumpdusty@gmail.com>
2021-07-02 11:42:18 -0700

    definitions changes

--- Project_6_Regression/proj_6_main.py
+++ Project_6_Regression/proj_6_main.py
@@ -16,18 +16,14 @@ import numpy as np
 
 women_employment=data("Mroz") 
 
-#Creating binary variable 
-
-women_employment['hoursw>0']=np.where(women_employment['hoursw']>0, 1, 0)
-
-
 #Building the plot
 
 (ggplot(women_employment)+
 geom_point(aes(x='hoursh', y='hoursw', color='child6')))
 
-#Hypothesis:
-#I wanna check if the "husband wage", "number of children under 6 years old" and "wife`s education" variables correlating with "wife`s hours of work" 
+#Null Hypothesis:
+#Variables named "husband wage", "number of children under 6 years old" and "wife`s education" will predict "wife`s hours of work"
+
 
 #Calculating OLS
 
@@ -37,9 +33,12 @@ est_hoursw.summary()
 #All of those variables are statistical significant
 
 #Magnitude for:
-    #"husband wage" is -28.7944 -that`s mean as more the husband earns as less wife working
+    #"husband wage" is -28.7944 - that`s mean as more the husband earns as less wife working
     #"number of children under 6 years old" is -392.7414 - presence of small children strongly descreasing wife`s working hours
     #"wife`s education" is +65.5197 - women with high education working more.
 
 #But the R-squared number  (0.084) does not explain our dependent variable strong enough
+
+
+
 
